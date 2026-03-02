e8f69d7da3c6115b213cf649a49bec7715eef286
bismillahkhanhn <bismillahkhan.hn@gmail.com>
2026-03-02 12:51:05 +0530

    updated

--- Phonebook.c
+++ Phonebook.c
@@ -6,71 +6,114 @@ struct Contact {
     char phone[15];
 };
 
-struct Contact contacts[100];
-int count = 0;
-
 void addContact() {
+    struct Contact c;
+    FILE *fp;
+
+    fp = fopen("contacts.dat", "ab"); // append binary
+    if (fp == NULL) {
+        printf("File error!\n");
+        return;
+    }
+
     printf("Enter name: ");
-    scanf("%s", contacts[count].name);
+    scanf("%s", c.name);
     printf("Enter phone number: ");
-    scanf("%s", contacts[count].phone);
-    count++;
-    printf("Contact added!\n");
+    scanf("%s", c.phone);
+
+    fwrite(&c, sizeof(c), 1, fp);
+    fclose(fp);
+
+    printf("Contact added successfully!\n");
 }
 
 void viewContacts() {
-    if (count == 0) {
+    struct Contact c;
+    FILE *fp;
+
+    fp = fopen("contacts.dat", "rb");
+    if (fp == NULL) {
         printf("No contacts found.\n");
         return;
     }
+
     printf("\n--- Contact List ---\n");
-    for (int i = 0; i < count; i++) {
-        printf("%d. %s - %s\n", i + 1, contacts[i].name, contacts[i].phone);
+    while (fread(&c, sizeof(c), 1, fp)) {
+        printf("%s - %s\n", c.name, c.phone);
     }
+
+    fclose(fp);
 }
 
 void searchContact() {
+    struct Contact c;
+    FILE *fp;
     char name[50];
     int found = 0;
+
     printf("Enter name to search: ");
     scanf("%s", name);
 
-    for (int i = 0; i < count; i++) {
-        if (strcmp(contacts[i].name, name) == 0) {
-            printf("Contact found: %s - %s\n", contacts[i].name, contacts[i].phone);
+    fp = fopen("contacts.dat", "rb");
+    if (fp == NULL) {
+        printf("No contacts found.\n");
+        return;
+    }
+
+    while (fread(&c, sizeof(c), 1, fp)) {
+        if (strcmp(c.name, name) == 0) {
+            printf("Contact Found: %s - %s\n", c.name, c.phone);
             found = 1;
             break;
         }
     }
+
+    fclose(fp);
 
     if (!found)
         printf("Contact not found.\n");
 }
 
 void deleteContact() {
+    struct Contact c;
+    FILE *fp, *temp;
     char name[50];
     int found = 0;
+
     printf("Enter name to delete: ");
     scanf("%s", name);
 
-    for (int i = 0; i < count; i++) {
-        if (strcmp(contacts[i].name, name) == 0) {
-            for (int j = i; j < count - 1; j++) {
-                contacts[j] = contacts[j + 1];
-            }
-            count--;
+    fp = fopen("contacts.dat", "rb");
+    temp = fopen("temp.dat", "wb");
+
+    if (fp == NULL || temp == NULL) {
+        printf("File error!\n");
+        return;
+    }
+
+    while (fread(&c, sizeof(c), 1, fp)) {
+        if (strcmp(c.name, name) != 0) {
+            fwrite(&c, sizeof(c), 1, temp);
+        } else {
             found = 1;
-            printf("Contact deleted.\n");
-            break;
         }
     }
 
-    if (!found)
+    fclose(fp);
+    fclose(temp);
+
+    remove("contacts.dat");
+    rename("temp.dat", "contacts.dat");
+
+    if (found)
+        printf("Contact deleted successfully!\n");
+    else
         printf("Contact not found.\n");
 }
 
 int main() {
     int choice;
+
     while (1) {
         printf("\n=== PHONE BOOK ===\n");
         printf("1. Add Contact\n");
@@ -86,7 +129,7 @@ int main() {
             case 2: viewContacts(); break;
             case 3: searchContact(); break;
             case 4: deleteContact(); break;
-            case 5: printf("Goodbye!\n"); return 0;
+            case 5: return 0;
             default: printf("Invalid choice!\n");
         }
     }
